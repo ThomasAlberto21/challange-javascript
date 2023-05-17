3561db177e6d4123c51f7e84cb791e399e7564a5
ThomasAlberto21 <thomasalberto456@gmail.com>
2023-05-17 19:52:36 +0700

    LATIHAN 4

---
+++ Latihan_4.js
@@ -0,0 +1,16 @@
+// Todo
+/*
+ * Buat program yang menampilkan bilangan ganjil di antara dua bilangan yang diberikan
+ */
+
+/*
+    Langkah-langkah
+
+    * Buat sebuah function tampilBilanganGanjil dan menerima 2 parameter (bilangan awal , bilangan akhir)
+    * 
+
+*/
+
+function tampilBilanganGanjil(bilangan_awal , bilangan_akhir){
+
+}
