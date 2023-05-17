6e7b1b75a7d8f2a8b1a01ed0688381bb3d0140ba
ThomasAlberto21 <thomasalberto456@gmail.com>
2023-05-17 20:06:24 +0700

    LATIHAN 4

--- Latihan_4.js
+++ Latihan_4.js
@@ -6,11 +6,19 @@
 /*
     Langkah-langkah
 
-    * Buat sebuah function tampilBilanganGanjil dan menerima 2 parameter (bilangan awal , bilangan akhir)
-    * 
+    * Buat sebuah function tampilBilanganGanjil dan menerima 2 parameter (bilangan_awal , bilangan_akhir).
+    * Buat perulangan for untuk memeriksa setiap bilangan dalam rentang tersebut. 
+    * Buat kondisi Jika bilangan tersebut tidak habis dibagi dua (bilangan % 2 !== 0) maka tampilkan bilangan 
+    * Panggil function tampilBilanganGanjil dan masukkan bilangan awal dan akhir di parameternya
 
 */
 
-function tampilBilanganGanjil(bilangan_awal , bilangan_akhir){
-
+function tampilBilanganGanjil(bilangan_awal, bilangan_akhir) {
+  for (let bilangan = bilangan_awal; bilangan <= bilangan_akhir; bilangan++) {
+    if (bilangan % 2 !== 0) {
+      console.log(bilangan);
+    }
+  }
 }
+
+tampilBilanganGanjil(1, 20);
